84e5a72ecfd333bfc7f4241ee5561a8cba94d6ea
Adi Nur Khamim <adinurkhamim@gmail.com>
2026-08-06 20:03:10 +0700

    fix: add top padding to ProjectDetailSidebar so Overview clears the web header zone

--- components/flow/projects/project-detail/ProjectDetailSidebar.tsx
+++ components/flow/projects/project-detail/ProjectDetailSidebar.tsx
@@ -115,7 +115,7 @@ export default function ProjectDetailSidebar() {
     <>
       {/* DESKTOP SIDEBAR */}
       <aside className="w-full hidden lg:flex flex-col">
-        <div className="space-y-4 pt-0">
+        <div className="space-y-4 pt-10">
           {/* Main Nav Items */}
           <div className="space-y-0.5">
             {/* Overview */}
